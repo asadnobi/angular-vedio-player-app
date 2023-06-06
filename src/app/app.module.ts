32b9732a5bdf0daa4a3eddd1fdd1350a8d557c13
Asadul Islam <asad@ussbdpos.com>
2023-06-06 18:25:24 +0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
